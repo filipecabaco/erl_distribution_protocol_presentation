f543114a69002a67d32e2620fedafafa6c5e1361
filipecabaco <filipecabaco@gmail.com>
2018-12-02 23:24:33 +0000

    Registry saves entry on message received

--- registry/registry.py
+++ registry/registry.py
@@ -1,18 +1,24 @@
-from pyrlang import Node, Process, GeventEngine
-from pyrlang.net_kernel import NetKernel
-from term import Atom
-from gevent import monkey
 import subprocess
 import logging
+import sqlite3
+from pyrlang import Node, Process, GeventEngine
+from term import Atom
+from gevent import monkey
 
 monkey.patch_socket(dns=True, aggressive=True)
 logging.getLogger("").setLevel(logging.DEBUG)
+
+conn = sqlite3.connect('registry')
+c = conn.cursor()
+c.execute('CREATE TABLE IF NOT EXISTS registry (device text, message text)')
 
 def remote_receiver_name():
   return Atom('orchestrator@localhost'), Atom("orchestrator")
 
 def send_message(node, pid, msg):
   logging.info("Sending back to orchestrator")
+  logging.info(msg[0])
+  c.execute(f'INSERT INTO registry(device, message) VALUES ("{msg[0]}","{msg[1]}")')
   node.send(sender=pid,
               receiver=remote_receiver_name(),
               message=msg)
@@ -31,9 +37,10 @@ class Register(Process):
 
 def main():
   e = GeventEngine()
+
   node = Node(node_name="registry@localhost", cookie="secret", engine=e)
-  logging.debug(node)
   Register(node)
+
   e.run_forever()
 
 if __name__ == "__main__":
